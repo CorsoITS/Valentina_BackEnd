4af4f1bd5ae17df593a7aaa93aaa1e2aedaf6b6d
Vale221090 <valentina.dv2210@gmail.com>
2022-04-22 12:03:45 +0200

    impostazione lavoro

---
+++ controllers/PersonaController.js
@@ -0,0 +1,30 @@
+const { Persona } = require("../model/models/Persona");
+
+class PersonaController {
+    static async lista (req, res) {
+        // console.log('trying operatore controller...')
+        if (req.query.q) {
+            if (!req.params) req.params = {};
+            req.params.id = req.query.q;
+            return PersonaController.length(req, res);
+        }
+
+        let result = await Persona.lista();
+        return res.json(result);
+    }
+
+    static async crea (req, res) {
+        let np = new Persona();
+        if (req.body.nome) np.setNome(req.body.nome);
+        if (req.body.cognome) np.setCognome(req.body.cognome);
+        if (req.body.codice_fiscale) np.setCodFis(req.body.codice_fiscale);
+        await np.save();
+        return res.json({
+            message: 'New User Created Successfully!!'
+        }); 
+    }
+}
+
+module.exports = {
+    PersonaController
+}
